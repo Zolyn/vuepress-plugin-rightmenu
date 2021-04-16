9be4d63fd413e62fc772a1cc1fe3497cb000aac3
PikaSama <pikasama@qq.com>
2021-04-17 00:19:37 +0800

    update

--- src/ContextMenu.vue
+++ src/ContextMenu.vue
@@ -3,12 +3,13 @@
         <v-menu
             v-model="showMenu"
             transition="slide-x-transition"
+            min-width="160px"
             offset-y
             absolute
             :position-x="xPos"
             :position-y="yPos"
-            :close-on-click="closeOnClick"
-            :close-on-content-click="closeOnContentClick"
+            :close-on-click="clickListener"
+            :close-on-content-click="clickListener"
         >
             <v-list>
                 <v-list-item>
@@ -18,16 +19,20 @@
                                 <v-icon
                                     v-text="item.icon"
                                     :class="{ 'on-hover': hover }"
-                                    @click="item.handler"
+                                    @click="item.handler.call(that)"
                                 ></v-icon>
                             </v-hover>
                         </v-col>
                     </v-row>
                 </v-list-item>
                 <v-divider></v-divider>
-                <v-list-item @click="logItem(item.title)" v-for="(item, index) in items" :key="index">
-                    <v-list-item-title>{{ item.title }}</v-list-item-title>
-                </v-list-item>
+                <v-row dense align="start" justify="center">
+                    <v-col>
+                        <v-list-item @click="logItem(item.title)" v-for="(item, index) in items.current" :key="items.type + index">
+                            <v-list-item-title>{{ item.title }}</v-list-item-title>
+                        </v-list-item>
+                    </v-col>
+                </v-row>
             </v-list>
         </v-menu>
     </v-app>
@@ -37,10 +42,17 @@
 export default {
     data() {
         return {
+            that: this,
             showMenu: false,
             xPos: 0,
             yPos: 0,
-            items: [{ title: 'Test 1' }, { title: 'Test 2' }, { title: 'Test 3' }, { title: 'Test 4' }],
+            items: {
+                type: 'normal',
+                normal: [{title: 'Test 1'}, {title: 'Test 2'}, {title: 'Test 3'}, {title: 'Test 4'}],
+                text: [{title: 'Text 1'}, {title: 'Text 2'}, {title: 'Text 3'}, {title: 'Text 4'}],
+                image: [{title: 'Img 1'}, {title: 'Img 2'}, {title: 'Img 3'}, {title: 'Img 4'}],
+                current: this.normal
+            },
             icons: [
                 {
                     icon: 'mdi-arrow-left',
@@ -57,7 +69,9 @@ export default {
                 {
                     icon: 'mdi-home',
                     handler() {
-                        this.$router.push('/');
+                        if (this.$route.path !== '/') {
+                            this.$router.push('/');
+                        }
                     },
                 },
                 {
@@ -67,9 +81,8 @@ export default {
                     },
                 },
             ],
-            closeOnClick: false,
-            closeOnContentClick: false,
-        };
+            clickListener: false
+        }
     },
     mounted() {
         this.$nextTick(() => {
@@ -86,10 +99,21 @@ export default {
     },
     methods: {
         handleClick(e) {
+            let items = this.items;
             e.preventDefault();
             this.showMenu = false;
             this.xPos = e.clientX;
             this.yPos = e.clientY;
+            if (e.target.innerText) {
+                items.type = 'text';
+                items.current = items.text;
+            } else if (e.target.nodeName === 'IMG') {
+                items.type = 'image';
+                items.current = items.image;
+            } else {
+                items.type = 'normal';
+                items.current = items.normal;
+            }
             this.$nextTick(() => {
                 this.showMenu = true;
             });
