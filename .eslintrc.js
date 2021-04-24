82fec0db5871f5c6ce802aaa508a2e219a5f4a1c
PikaSama <pikasama@qq.com>
2021-04-24 15:39:22 +0800

    feat: custom configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
     extends: ['alloy', 'alloy/vue'],
     env: {},
+    globals: {
+        ICON_BAR: true,
+        NORMAL_ACTIONS: true,
+        EVENT_ACTIONS: true,
+        STICKY_ACTIONS: true,
+    },
     rules: {
         'vue/component-tags-order': [
             'error',
--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const { resolve } = require('path');
-module.exports = () => ({
+module.exports = (opts) => ({
+    define() {
+        return {
+            ICON_BAR: opts.iconBar,
+            NORMAL_ACTIONS: opts.normalActions,
+            EVENT_ACTIONS: opts.eventActions,
+            STICKY_ACTIONS: opts.stickyActions,
+        };
+    },
     name: '@Zolyn/vuepress-plugin-rightmenu',
     enhanceAppFiles: resolve(__dirname, './src/enhanceAppFile.js'),
     globalUIComponents: 'ContextMenu',
