3e3acd9e584829f8d8c32aae7dad5001dc132f38
Zorin <pikasama@qq.com>
2021-05-23 13:52:17 +0800

    style: menu contents border

--- src/MenuItem.vue
+++ src/MenuItem.vue
@@ -2,16 +2,18 @@
     <v-app id="context-menu-item">
         <div v-for="(item, index) in list" :key="name + index" class="pure-g justify-start align-center">
             <div class="pure-u-1-1">
-                <v-list-item :dense="dense" @click="item.handler.call(parent)">
-                    <v-list-item-icon v-if="item.icon">
-                        <v-icon v-text="item.icon"></v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
-                        <v-list-item-title>
+                <v-hover v-slot="{ hover }">
+                    <v-list-item :dense="dense" @click="item.handler.call(parent)">
+                        <v-icon
+                            v-if="item.icon"
+                            :class="{ 'on-hover': hover, 'pure-gutter': true }"
+                            v-text="item.icon"
+                        ></v-icon>
+                        <v-list-item-title :class="{ 'on-hover': hover, 'pure-gutter': true }">
                             {{ item.title }}
                         </v-list-item-title>
-                    </v-list-item-content>
-                </v-list-item>
+                    </v-list-item>
+                </v-hover>
             </div>
         </div>
     </v-app>
